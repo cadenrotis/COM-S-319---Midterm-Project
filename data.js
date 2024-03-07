aac20e6bc8164aee906d4a72139c8ca78856832c
Caden Otis <cadenotis@gmail.com>
2024-03-06 23:08:18 -0600

    Finished Fall of Rome page

--- data.js
+++ data.js
@@ -4,50 +4,56 @@ fetch("./data.json")
 
 function load(imageResult)
 {
-    let img1 = document.getElementById("crisisofThirdCenImg");
-    let text1 = document.getElementById("crisisofThirdCenImgDescription");
-    let title1 = document.getElementById("crisisofThirdCenImgTitle");
+    // punic wars timeline elements
+    
+    
+    // crisis of the third century elements
+    let img4 = document.getElementById("crisisofThirdCenImg");
+    let text4 = document.getElementById("crisisofThirdCenImgDescription");
+    let title4 = document.getElementById("crisisofThirdCenImgTitle");
 
-    let img2 = document.getElementById("riseOfEasternEmpireImg");
-    let text2 = document.getElementById("riseOfEasternEmpireImgDescription");
-    let title2 = document.getElementById("riseOfEasternEmpireImgTitle");
+    // rise of the eastern empire elements
+    let img5 = document.getElementById("riseOfEasternEmpireImg");
+    let text5 = document.getElementById("riseOfEasternEmpireImgDescription");
+    let title5 = document.getElementById("riseOfEasternEmpireImgTitle");
 
-    let img3 = document.getElementById("fallOfRomeImg");
-    let text3 = document.getElementById("fallOfRomeImgDescription");
-    let title3 = document.getElementById("fallOfRomeImgTitle");
+    // fall of rome elements
+    let img6 = document.getElementById("fallOfRomeImg");
+    let text6 = document.getElementById("fallOfRomeImgDescription");
+    let title6 = document.getElementById("fallOfRomeImgTitle");
     
     for (var i = 0; i < imageResult.images.length; i++) {
-        // load the images and text out
         let img = imageResult.images[i].image;
         let textt = imageResult.images[i].text;
         let title = imageResult.images[i].titleImg;
 
-        // create div for picture
+        // get the image and put into a div element
         let imge = document.createElement("div");
-        imge.innerHTML = `<img src=${img}  alt=${title} height="300" style="margin-top: 100px; margin-left:50px"></img>`;
+        imge.innerHTML = `<img src=${img}  alt=${title} height="300" style="margin-top: 100px; margin-left:30px"></img>`;
 
-        // create p for text
+        // get text information from the JSON file and put into a p element
         let txt = document.createElement("p");
         txt.innerHTML = `<p class="lead">${textt}</p>`;
 
-        // create something for title
+        // get title information from the JSON file and put into a h2 element
         let subject = document.createElement("h2");
         subject.innerHTML = `<h2 class="featurette-heading fw-normal lh-1"><strong>${title}</strong></h2>`;
 
+        // place images, text, and descriptions onto webpages
         if (imageResult.images[i].imageId === "crisis") {
-            img1.appendChild(imge);
-            text1.appendChild(txt);
-            title1.appendChild(subject);
+            img4.appendChild(imge);
+            text4.appendChild(txt);
+            title4.appendChild(subject);
         }
         else if (imageResult.images[i].imageId === "rise") {
-            img2.appendChild(imge);
-            text2.appendChild(txt);
-            title2.appendChild(subject);
+            img5.appendChild(imge);
+            text5.appendChild(txt);
+            title5.appendChild(subject);
         }
         else if (imageResult.images[i].imageId === "fall") {
-            img3.appendChild(imge);
-            text3.appendChild(txt);
-            title3.appendChild(subject);
+            img6.appendChild(imge);
+            text6.appendChild(txt);
+            title6.appendChild(subject);
         }
-    } // end for loop
+    } 
 }
